f00aa5104154d58ba9c25523cb9e503c45ba4457
dmitriiif <dmitriicalpe2005@gmail.com>
2024-05-21 12:24:21 +0200

    Updated the base prefixes

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -141,11 +141,11 @@ public:
         vSeeds.emplace_back("dnsseed.emzy.de."); // Stephan Oeste
         vSeeds.emplace_back("seed.testcoin.wiz.biz."); // Jason Maurice
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,0);
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
-        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,128);
-        base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
-        base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,65);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,80);
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,69);
+        base58Prefixes[EXT_PUBLIC_KEY] = {0x0D, 0x88, 0xB2, 0x1E};
+        base58Prefixes[EXT_SECRET_KEY] = {0x0D, 0x88, 0xAD, 0xE4};
 
         bech32_hrp = "bc";
 
